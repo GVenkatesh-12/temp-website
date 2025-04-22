9e15563f760b99cae4b698549da6e73fc091b876
gvenkatesh-12 <gvenkatesh.on@gmail.com>
2025-04-22 23:11:15 +0530

    modified

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -8,11 +8,11 @@ const Hero: React.FC = () => {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-[50vh] py-4 sm:py-6">
 
-      <div className="w-full flex justify-center items-center px-4 sm:px-6 mt-20 md:mt-16">
+      <div className="w-full flex justify-center items-center px-4 sm:px-6 mt-12 md:mt-16">
         <div className="relative z-10 max-w-4xl text-center">
           <h1 className="text-2xl sm:text-3xl lg:text-5xl font-bold text-white mb-2 md:mb-3 leading-tight">
             <span className="inline-block">2025 IEEE 3rd International</span>{' '}
-            <span className="inline-block">Symposium on Sustainable Energy, </span>{' '}
+            <span className="inline-block">Symposium on Sustainable Energy,</span>{' '}
             <span className="inline-block">Signal Processing and Cybersecurity</span>
           </h1>
 
