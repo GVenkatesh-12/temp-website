72a87bfb9c9d24d3be7132f541593d99b56ae2bd
gvenkatesh-12 <gvenkatesh.on@gmail.com>
2025-05-24 19:45:43 +0530

    added 4 tpc

--- src/pages/committees/TechnicalProgram.tsx
+++ src/pages/committees/TechnicalProgram.tsx
@@ -206,7 +206,12 @@ const TechnicalProgram = () => {
               { name: "Dr. Amit Ranjan Azad", affiliation: "IIIT Kalyani" },
               { name: "Dr. Amit Kumar", affiliation: "IIIT Kota" },
               { name: "Dr. Rajanish Kumar Singh", affiliation: "IIIT Nagpur" },
-              { name: "Dr. Rudranarayan Pradhan", affiliation: "OUTR Bhubaneswar" }
+              { name: "Dr. Rudranarayan Pradhan", affiliation: "OUTR Bhubaneswar" },
+              { name: "Dr. Mangesh Kose", affiliation: "IIIT Nagpur" },
+              { name: "Dr. Shankar Bhattachajee", affiliation: "IIIT Nagpur" },
+              { name: "Dr. Khushboo Jain", affiliation: "IIIT Nagpur" },
+              { name: "Dr. Gourav Dubey", affiliation: "IIITM Gwalior" }
+
             ].map((member, index) => (
               <div
                 key={index}
